baf666510a793fb25ea936be0558e4625015e0fc
Rodrigo Gomez Avila <rogomez@renalpathsoc.org>
2016-12-14 23:32:33 -0800

    Problem 9 Part B

--- 2016/solutions/9.rb
+++ 2016/solutions/9.rb
@@ -54,8 +54,35 @@ class Problem9 < Problem
   end
 
   def part_b
+    result = 0
     self.input.each_line do |line|
-      # Process input here
+      result = count_characters_on(line)
     end
+    result
+  end
+
+  def count_characters_on(string)
+    returnCount = 0
+    i = 0
+
+    while i < string.size
+      if (string[i].eql? '(')
+        marker = string[i..string.index(')', i)]
+        target_substring = string[(string.index(')', i) + 1)..(string.size-1)]
+        # puts "Marker: #{marker}, Target Substring: #{target_substring}"
+        if /\A\((?<num_chars>\d+)x(?<times>\d+)\)\Z/ =~ marker
+          num_chars = Integer(num_chars)
+          times = Integer(times)
+          returnCount += times * count_characters_on(target_substring[0..(num_chars-1)])
+          i += marker.size + num_chars
+        else
+          raise "Ill-formed repetition marker: #{marker}"
+        end
+      else
+        returnCount += 1
+        i += 1
+      end
+    end
+    returnCount
   end
 end
--- 2016/spec/problem9_spec.rb
+++ 2016/spec/problem9_spec.rb
@@ -41,42 +41,39 @@ describe Problem9, :Problem9 do
     it "returns right answer for input" do
       p = Problem9.new
       p.load_input_from_file './input/9.txt'
-      expect(p.part_a).to eql(nil)
+      expect(p.part_a).to eql(123908)
     end
   end
 
   describe "part_b" do
-    skip ("Until it is implemented")
     it "returns right answer for example 1" do
-      p = Problem7.new
-      p.input = 'aba[bab]xyz'
-      expect(p.part_b).to eql(1)
+      p = Problem9.new
+      p.input = '(3x3)XYZ'
+      expect(p.part_b).to eql('XYZXYZXYZ'.size)
     end
 
     it "returns right answer for example 2" do
-      p = Problem7.new
-      p.input = 'xyx[xyx]xyx'
-      expect(p.part_b).to eql(0)
+      p = Problem9.new
+      p.input = 'X(8x2)(3x3)ABCY'
+      expect(p.part_b).to eql('XABCABCABCABCABCABCY'.size)
     end
 
     it "returns right answer for example 3" do
-      p = Problem7.new
-      p.input = 'aaa[kek]eke'
-      expect(p.part_b).to eql(1)
+      p = Problem9.new
+      p.input = '(27x12)(20x12)(13x14)(7x10)(1x12)A'
+      expect(p.part_b).to eql(241920)
     end
 
     it "returns right answer for example 4" do
-      p = Problem7.new
-      p.input = 'zazbz[bzb]cdb'
-      expect(p.part_b).to eql(1)
+      p = Problem9.new
+      p.input = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
+      expect(p.part_b).to eql(445)
     end
 
     it "returns right answer for input" do
-      # skip("While testing")
-      p = Problem7.new
-      p.load_input_from_file './input/7.txt'
-      expect(p.part_b).to eql(258)
+      p = Problem9.new
+      p.load_input_from_file './input/9.txt'
+      expect(p.part_b).to eql(10755693147)
     end
   end
-
 end
