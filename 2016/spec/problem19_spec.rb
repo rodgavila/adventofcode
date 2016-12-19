0173faaafe690c18a95aa39c561d1bfb622dff19
Rodrigo Gomez Avila <rogomez@renalpathsoc.org>
2016-12-19 04:07:29 -0800

    Solution to Problem 19 Part A

---
+++ 2016/spec/problem19_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../solutions/19.rb'
+
+describe Problem19, :Problem19 do
+  describe "part_a", :skip do
+    it "returns right answer for example" do
+      p = Problem19.new
+      p.input = 5
+      expect(p.part_a).to eql(3)
+    end
+
+    it "returns right answer for input" do
+      p = Problem19.new
+      p.input = 3001330
+      expect(p.part_a).to eql(1808357)
+    end
+  end
+
+  describe "part_b" do
+    it "returns right answer for example" do
+      p = Problem19.new
+      p.input = 5
+      expect(p.part_b).to eql(2)
+    end
+
+    it "returns right answer for input" do
+      p = Problem19.new
+      p.input = 3001330
+      expect(p.part_b).to eql(-1)
+    end
+  end
+end
